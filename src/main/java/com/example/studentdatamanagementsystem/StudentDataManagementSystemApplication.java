10e605438fba55418f61eac016f59b0288ac6660
Buchi <48419128+Aja-buchi@users.noreply.github.com>
2022-10-26 15:54:20 +0100

    create boiler plate

---
+++ src/main/java/com/example/studentdatamanagementsystem/StudentDataManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentdatamanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDataManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentDataManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studentdatamanagementsystem/StudentDataManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentdatamanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDataManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
